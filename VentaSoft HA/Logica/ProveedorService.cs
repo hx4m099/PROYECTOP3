6b81c1898abc058a86292621ba1cc22b70f10bf9
Mincho1107 <ajosepineda@unicesar.edu.co>
2025-05-21 15:55:15 -0500

    FINALIZACION CAPA LOGICA

---
+++ VentaSoft HA/Logica/ProveedorService.cs
@@ -0,0 +1,93 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logica
+{
+    internal class ProveedorService
+    {
+        private CD_Proveedor objcd_Proveedor = new CD_Proveedor();
+
+
+        public List<Proveedor> Listar()
+        {
+            return objcd_Proveedor.Listar();
+        }
+
+        public int Registrar(Proveedor obj, out string Mensaje)
+        {
+            Mensaje = string.Empty;
+
+            if (obj.Documento == "")
+            {
+                Mensaje += "Es necesario el documento del Proveedor\n";
+            }
+
+            if (obj.RazonSocial == "")
+            {
+                Mensaje += "Es necesario la razon social del Proveedor\n";
+            }
+
+            if (obj.Correo == "")
+            {
+                Mensaje += "Es necesario la correo del Proveedor\n";
+            }
+
+            if (Mensaje != string.Empty)
+            {
+                return 0;
+            }
+            else
+            {
+                return objcd_Proveedor.Registrar(obj, out Mensaje);
+            }
+
+
+        }
+
+
+        public bool Editar(Proveedor obj, out string Mensaje)
+        {
+
+            Mensaje = string.Empty;
+
+            if (obj.Documento == "")
+            {
+                Mensaje += "Es necesario el documento del Proveedor\n";
+            }
+
+            if (obj.RazonSocial == "")
+            {
+                Mensaje += "Es necesario la razon social del Proveedor\n";
+            }
+
+            if (obj.Correo == "")
+            {
+                Mensaje += "Es necesario la correo del Proveedor\n";
+            }
+
+
+
+            if (Mensaje != string.Empty)
+            {
+                return false;
+            }
+            else
+            {
+                return objcd_Proveedor.Editar(obj, out Mensaje);
+            }
+
+
+        }
+
+
+        public bool Eliminar(Proveedor obj, out string Mensaje)
+        {
+            return objcd_Proveedor.Eliminar(obj, out Mensaje);
+        }
+
+    }
+}
